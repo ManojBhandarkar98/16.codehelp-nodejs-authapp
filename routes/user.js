ce5c12df18575b12ed8a49b18b5e52b9df04954d
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-27 08:19:25 +0530

    Episode - 8 -Authentication & Authorization - Protected Middleware - to check role and restrict permission user wise

--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,32 @@ const router = express.Router();
 
 //import controller
 const { signup, signin } = require("../controllers/Auth");
+//midleware get
+const {auth, isStudent, isAdmin} = require("../middlewares/auth");
 
 router.post("/login", signin);
 router.post("/signup", signup);
 
+//Testing protected route for single middleware
+router.get("/test", auth, (req,res)=>{
+    res.json({
+        sucess:true,
+        message:"Welcome to the protect route for test"
+    });
+})
 
-
+//protected route means role wise
+router.get("/student", auth,isStudent, (req,res)=>{
+    res.json({
+        sucess:true,
+        message:"Welcome to the protect route for student"
+    });
+})
+router.get("/admin", auth,isStudent, (req,res)=>{
+    res.json({
+        sucess:true,
+        message:"Welcome to the protect route for Admin"
+    });
+});
 
 module.exports = router;
